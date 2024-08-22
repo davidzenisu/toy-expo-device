14c34aff18374f569388af0eec03b137880f2f6c
David Zeni-Su <david.zeni-su@oebb.at>
2024-08-22 09:36:58 +0000

    Add npm i to postCreateCommand

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,8 @@ echo -e "\nStarting post creat command script..."
 echo "Dev machine:"
 uname -a
 echo -e "\nInstalling expo boiler plate..."
-npm install -g -y create-expo-app@latest
+# npm i -g create-expo-app@latest
+npm install
 
 echo -e "\n*******************************"
 echo -e "\nDev container ready!".
