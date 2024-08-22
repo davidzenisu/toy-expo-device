b19fd7aff49704af8eca7aad96728eca57b4b354
David Zeni-Su <david.zeni-su@oebb.at>
2024-08-22 12:56:07 +0000

    Add comment to postCreatCommand

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,9 +2,10 @@ echo -e "\nStarting post creat command script..."
 echo "Dev machine:"
 uname -a
 echo -e "\nInstalling expo boiler plate..."
-# npm i -g create-expo-app@latest
-npm install
+# npm i -g create-expo-app@latest # only required for project setup
 npm i -g @expo/ngrok
+# npm install -g eas-cli # not used
+npm install
 
 echo -e "\n*******************************"
 echo -e "\nDev container ready!".
