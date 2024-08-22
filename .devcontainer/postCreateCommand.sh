f9fd097cb2e0846422a31dbeb3c7bb3e458141aa
David Zeni-Su <david.zeni-su@oebb.at>
2024-08-22 08:23:41 +0000

    Add Expo template

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+echo -e "\nStarting post creat command script..."
+echo "Dev machine:"
+uname -a
+echo -e "\nInstalling expo boiler plate..."
+npm install -g -y create-expo-app@latest
+
+echo -e "\n*******************************"
+echo -e "\nDev container ready!".
+echo -e "\n*******************************\n"
